69603d5b2f378f93b43c91d64c2fb9b699ce4505
coopfeathy <cooperfeatherstone13@gmail.com>
2024-03-03 11:27:59 -0600

    Download the DocumentEditComponent HTML and CSS files

--- cms-new/src/app/documents/document-edit/document-edit.component.ts
+++ cms-new/src/app/documents/document-edit/document-edit.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { Document } from '../document.model';
 @Component({
   selector: 'app-document-edit',
   templateUrl: './document-edit.component.html',
   styleUrl: './document-edit.component.css'
 })
 export class DocumentEditComponent {
+  document: Document;
 
+  onCancel() {
+    // Add your logic here
+  }
 }
