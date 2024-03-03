f91248793348e90631d163d742ecc7444bde9f6e
coopfeathy <cooperfeatherstone13@gmail.com>
2024-03-03 12:19:21 -0600

    Implementing the Behavior of the Document Edit Form

--- cms-new/src/app/documents/document-edit/document-edit.component.ts
+++ cms-new/src/app/documents/document-edit/document-edit.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Document } from '../document.model';
 import { NgForm } from '@angular/forms';
+import { DocumentService } from '../document.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 @Component({
   selector: 'app-document-edit',
   templateUrl: './document-edit.component.html',
@@ -11,11 +13,42 @@ export class DocumentEditComponent {
   document: Document;
   editMode: boolean = false;
 
+  constructor(
+    private documentService: DocumentService,
+    private router: Router,
+    private route: ActivatedRoute) {
+  }
+
   onSubmit(form: NgForm) {
-    // Add your logic here
+    const value = form.value;
+    const newDocument = new Document(value.id, value.name, value.desciption, value.url);
+    if (this.editMode) {
+      this.documentService.updateDocument(this.originalDocument, newDocument);
+    } else {
+      this.documentService.addDocument(newDocument);
+    }
+    this.router.navigate(['/documents']);
   }
 
   onCancel() {
-    // Add your logic here
+    this.router.navigate(['/documents']);
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        const id = params['id'];
+        if (!id) {
+          this.editMode = false;
+          return;
+        }
+        this.originalDocument = this.documentService.getDocument(id);
+        if (!this.originalDocument) {
+          return;
+        }
+        this.editMode = true;
+        this.document = JSON.parse(JSON.stringify(this.originalDocument));
+      }
+    );
   }
 }
