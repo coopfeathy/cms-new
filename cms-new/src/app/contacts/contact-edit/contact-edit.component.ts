68a3070b0fccf82ac04c87fc124efb1a68d2d2c8
coopfeathy <cooperfeatherstone13@gmail.com>
2024-03-03 18:42:23 -0600

    Implementing the Behavior of the Contact Edit Form

--- cms-new/src/app/contacts/contact-edit/contact-edit.component.ts
+++ cms-new/src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Contact } from '../contact.model';
 import { NgForm } from '@angular/forms';
+import { ContactsService } from '../contacts.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-edit',
@@ -8,17 +10,51 @@ import { NgForm } from '@angular/forms';
   styleUrl: './contact-edit.component.css'
 })
 export class ContactEditComponent {
+  originalContact: Contact;
   contact: Contact;
-  groupContacts: Contact[] = []; 
+  groupContacts: Contact[] = [];
+  editMode: boolean = false;
+  id: string;
 
-  constructor() {
+  constructor(
+    private contactService: ContactsService,
+    private router: Router,
+    private route: ActivatedRoute) {
+      
   }
 
   onCancel() {
-
+    this.router.navigate(['/contacts']);
   }
 
   onSubmit(form: NgForm) {
-
+    const value = form.value;
+    const newContact = new Contact(value.id, value.name, value.email, value.phone, value.imageUrl, this.groupContacts);
+    if (this.editMode) {
+      this.contactService.updateContact(this.originalContact, newContact);
+    } else {
+      this.contactService.addContact(newContact);
+    }
+    this.router.navigate(['/contacts']);
   }
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = params['id'];
+      if (!this.id) {
+        this.editMode = false;
+        return;
+      }
+      this.originalContact = this.contactService.getContact(this.id);
+      if (!this.originalContact) {
+        return;
+      }
+      this.editMode = true;
+      this.contact = JSON.parse(JSON.stringify(this.originalContact));
+      if (this.originalContact.group) {
+        this.groupContacts = JSON.parse(JSON.stringify(this.originalContact.group));
+      }
+    });
+  }
+
 }
