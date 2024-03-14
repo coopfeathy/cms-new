97ccaba6a0c6801b00154cc093cb8730da359977
coopfeathy <cooperfeatherstone13@gmail.com>
2024-03-14 00:10:15 -0500

    Modify the MessageService to Perform HTTP Get/Put Requests

--- cms-new/src/app/messages/message-list/message-list.component.ts
+++ cms-new/src/app/messages/message-list/message-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Message } from '../message.model';
 import { MessageService } from '../message.service';
-
+import { Subscription } from 'rxjs/internal/Subscription';
+import { ContactsService } from '../../contacts/contacts.service';
 
 @Component({
   selector: 'app-message-list',
@@ -10,15 +11,19 @@ import { MessageService } from '../message.service';
 })
 export class MessageListComponent implements OnInit {
   messages: Message[] = [];
+  private subscription: Subscription
 
-  constructor(private messageService: MessageService) {}
+  constructor(private messageService: MessageService,
+    private contactService: ContactsService) {
+    this.messageService.getMessages()
+    this.contactService.getContacts()
+  }
 
   ngOnInit() {
-    this.messages = this.messageService.getMessages();
-    this.messageService.messageChangedEvent.subscribe(
-      (messages: Message[]) => {
-        this.messages = messages;
-      }
-    );
+    this.subscription = this.messageService.messageChangedEvent
+      .subscribe((messageList: Message[]) => {
+          this.messages = messageList
+        }
+      )
   }
 }
--- cms-new/src/app/messages/message.service.ts
+++ cms-new/src/app/messages/message.service.ts
@@ -1,6 +1,7 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Message } from './message.model';
 import { MOCKMESSAGES } from './MOCKMESSAGES';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +9,28 @@ import { MOCKMESSAGES } from './MOCKMESSAGES';
 export class MessageService {
   messageChangedEvent = new EventEmitter<Message[]>();
   private messages: Message[] = [];
+  private maxMessageId: number;
 
-  constructor() {
-    this.messages = MOCKMESSAGES;
+  constructor(private http: HttpClient) {
+    this.getMessages();
   }
 
   getMessages() {
-    return this.messages.slice();
+    this.http.get('https://cpf-cms-bf4f3-default-rtdb.firebaseio.com/messages.json')
+      .subscribe((messages: Message[]) => {
+        this.messages = messages;
+        this.maxMessageId = this.getMaxId();
+        this.messageChangedEvent.next(this.messages.slice());
+      });
+  }
+
+  storeMessages() {
+    const messages = JSON.stringify(this.messages);
+    const headers = new HttpHeaders({'Content-Type': 'application/json'});
+    this.http.put('https://cpf-cms-bf4f3-default-rtdb.firebaseio.com/messages.json', messages, {headers: headers})
+      .subscribe(() => {
+        this.messageChangedEvent.next(this.messages.slice());
+      });
   }
 
   getMessage(id: string): Message {
@@ -24,5 +40,17 @@ export class MessageService {
   addMessage(message: Message) {
     this.messages.push(message);
     this.messageChangedEvent.emit(this.messages.slice());
+    this.storeMessages();
+  }
+
+  getMaxId(): number {
+    let maxId = 0;
+    for (let message of this.messages) {
+      let currentId = parseInt(message.id);
+      if (currentId > maxId) {
+        maxId = currentId;
+      }
+    }
+    return maxId;
   }
 }
